1d5e131b87eb59db51f9479f884c52b44470e207
HuguesK <kouakouhugues804@gmail.com>
2022-12-31 14:07:57 +0100

    to proxy all requests

--- Frontend/app/src/setupProxy.js
+++ Frontend/app/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api', // You can pass in an array too eg. ['/api', '/another/path']
+    '/', // You can pass in an array too eg. ['/api', '/another/path']
     createProxyMiddleware({
       target: process.env.REACT_APP_SERVER,
       changeOrigin: true,
